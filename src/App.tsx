a9b6a139e65263bae5e6c8b0c3eee8cae3e128a8
Triioade <trioade29@gmail.com>
2025-06-14 19:25:14 +0700

    prrte

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/AuthPages/SignIn";
 import NotFound from "./pages/OtherPage/NotFound";
 import UserProfiles from "./pages/UserProfiles";
-
 import TaskPage from "./pages/task/Task";
 import Calendar from "./pages/Calendar";
 import SemesterUser from "./pages/Semester/SemesterUser";
@@ -16,50 +15,101 @@ import WeekUserPage from "./pages/Week/Weekuser";
 import TaskUserPage from "./pages/task/Taskuser";
 import EmailSidebar from "./pages/Email/EmailSidebar";
 import MatkulSidebar from "./pages/Matkul/MatkulSidebar";
+import ProtectedRoute from "./protectroute";
 
 export default function App() {
   return (
-    <>
-      <Router>
-        <ScrollToTop />
-        <Routes>
-          <Route index path="/" element={<SignIn />} />
-          <Route path="/signin" element={<SignIn />} />
-          {/* Dashboard Layout */}
-          <Route element={<AppLayout />}>
-          <Route path="/semester" element={<Semester />} />
-            <Route path="/week" element={<WeekSidebar />} />
-            <Route path="/week/:semesterId" element={<WeekPage />} />
-            <Route path="/task/:semesterId/:weekId" element={<TaskPage />} />
-            <Route path="/email" element={<EmailSidebar />} />
-            <Route path="/matkul" element={<MatkulSidebar />} />
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signin" element={<SignIn />} />
 
-            {/* User Test */}
-            <Route path="/semester-user" element={<SemesterUser />} />
-            <Route path="/week-user/:semesterId" element={<WeekUserPage />} />
-            <Route
-              path="/task-user/:semesterId/:weekId"
-              element={<TaskUserPage />}
-            />
+        {/* Protected Routes inside Dashboard Layout */}
+        <Route element={<AppLayout />}>
+          <Route
+            path="/semester"
+            element={
+              <ProtectedRoute>
+                <Semester />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/week"
+            element={
+              <ProtectedRoute>
+                <WeekSidebar />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/week/:semesterId"
+            element={
+              <ProtectedRoute>
+                <WeekPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/task/:semesterId/:weekId"
+            element={
+              <ProtectedRoute>
+                <TaskPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/email"
+            element={
+              <ProtectedRoute>
+                <EmailSidebar />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/matkul"
+            element={
+              <ProtectedRoute>
+                <MatkulSidebar />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/semester-user"
+            element={
+              <ProtectedRoute>
+                <SemesterUser />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/week-user/:semesterId"
+            element={
+              <ProtectedRoute>
+                <WeekUserPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/task-user/:semesterId/:weekId"
+            element={
+              <ProtectedRoute>
+                <TaskUserPage />
+              </ProtectedRoute>
+            }
+          />
 
-            {/* Dashboard */}
+          {/* Non-protected pages inside layout */}
+          <Route path="/profile" element={<UserProfiles />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/blank" element={<Blank />} />
+        </Route>
 
-            {/* Others Page */}
-            <Route path="/profile" element={<UserProfiles />} />
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="/blank" element={<Blank />} />
-
-            {/* Ui Elements */}
-          </Route>
-
-          {/* Auth Layout */}
-          {/* <Route index path="/" element={<SignIn />} />
-          <Route path="/signin" element={<SignIn />} /> */}
-
-          {/* Fallback Route */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </>
+        {/* Fallback */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
